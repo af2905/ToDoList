30a86bde209ab5c8c5ae567aaaae0753da5511de
af2905 <af2905g@gmail.com>
2020-03-13 12:19:10 +0300

    Added the ability to add images from the camera

--- app/src/main/java/ru/job4j/todolist/fragments/AddFragment.java
+++ app/src/main/java/ru/job4j/todolist/fragments/AddFragment.java
@@ -1,7 +1,9 @@
 package ru.job4j.todolist.fragments;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
@@ -9,8 +11,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,12 +23,15 @@ import ru.job4j.todolist.R;
 import ru.job4j.todolist.model.Item;
 import ru.job4j.todolist.store.SqlStore;
 
+import static android.app.Activity.RESULT_OK;
+
 public class AddFragment extends Fragment implements View.OnClickListener, TextWatcher {
-    private Button save;
     private EditText editName;
     private EditText editDesc;
-    private CheckBox done;
+    private ImageView photo;
+    private Button save;
     private SqlStore store;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Nullable
     @Override
@@ -38,6 +43,8 @@ public class AddFragment extends Fragment implements View.OnClickListener, TextW
         save = view.findViewById(R.id.save);
         save.setEnabled(false);
         save.setOnClickListener(this);
+        photo = view.findViewById(R.id.photo);
+        photo.setOnClickListener(this);
         editName.setFilters(new InputFilter[]{new InputFilter.LengthFilter(14)});
         editDesc.setFilters(new InputFilter[]{new InputFilter.LengthFilter(24)});
         editName.addTextChangedListener(this);
@@ -47,15 +54,37 @@ public class AddFragment extends Fragment implements View.OnClickListener, TextW
 
     @Override
     public void onClick(View v) {
-        String descText = editDesc.getText().toString();
-        if (descText.length() == 0) {
-            descText = "description not added";
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.save:
+                String descText = editDesc.getText().toString();
+                if (descText.length() == 0) {
+                    descText = "description not added";
+                }
+                store.addItem(new Item(editName.getText().toString(),
+                        descText,
+                        CalendarFormat.dateFormatMethod(), 0));
+                intent = new Intent(getActivity().getApplicationContext(), ItemsActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.photo:
+                intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                }
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + v.getId());
         }
-        store.addItem(new Item(editName.getText().toString(),
-                descText,
-                CalendarFormat.dateFormatMethod(), 0));
-        Intent intent = new Intent(getActivity().getApplicationContext(), ItemsActivity.class);
-        startActivity(intent);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            photo.setImageBitmap(imageBitmap);
+        }
     }
 
     @Override
